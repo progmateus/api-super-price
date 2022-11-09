0c86d983674a245aa0da35056030bf6ec4401b86
Mateus Lopes <73197343+progmateus@users.noreply.github.com>
2022-11-08 23:32:57 -0300

    ormconfig with environment variables

---
+++ ormconfig.js
@@ -0,0 +1,13 @@
+module.exports = {
+    "type": "postgres",
+    "port": "5432",
+    "host": "localhost",
+    "username": process.env.POSTGRES_USER,
+    "password": process.env.POSTGRES_PASSWORD,
+    "database": process.env.POSTGRES_DB,
+    "migrations": ["src/database/migrations/*.ts"],
+    "cli": {
+        "migrationsDir": "src/database/migrations"
+    },
+    "entities": ["./src/modules/**/entities/*.ts"]
+}
