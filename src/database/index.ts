2c0316b97219d5302853bdd3b81d20524fe85561
Mateus Lopes <73197343+progmateus@users.noreply.github.com>
2022-11-26 21:22:27 -0300

    Database Docker

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,11 +1,9 @@
 import { Connection, createConnection, getConnectionOptions } from "typeorm"
 
-export default async (host = "super_price_database"): Promise<Connection> => {
+export default async (): Promise<Connection> => {
     const defaultOptions = await getConnectionOptions()
     return createConnection(
         Object.assign(defaultOptions, {
-            host: process.env.NODE_ENV === "test" ? "localhost" : host,
-
             database: process.env.NODE_ENV === "test"
                 ? "super_price_test"
                 : defaultOptions.database
