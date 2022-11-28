1e9676b21dc5f9f2834252702b1b678de2432129
Mateus Lopes <73197343+progmateus@users.noreply.github.com>
2022-11-28 17:09:28 -0300

    allowed headers

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -17,12 +17,15 @@ import upload from "@config/upload";
 createConnection();
 const app = express();
 app.use(express.json())
+app.use(cors({
+    allowedHeaders: "Upgrade-Insecure-Requests: 1"
+}));
+
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`))
 
-app.use(cors());
 app.use(router)
 
 app.use(
